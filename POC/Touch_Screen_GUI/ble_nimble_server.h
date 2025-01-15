c23dce0261b96c69ad1c5fc36e2e671bd0f9e8c6
Avigail Yampolsky <157058510+avigailyam@users.noreply.github.com>
2025-01-15 20:09:41 +0200

    Add files via upload

--- POC/Touch_Screen_GUI/ble_nimble_server.h
+++ POC/Touch_Screen_GUI/ble_nimble_server.h
@@ -1,12 +1,11 @@
-#include "HardwareSerial.h"
-// Tue 01/07/2025 serverAck 
-
 #include <NimBLEDevice.h>
 #include <string>
+#include <atomic>
 
-static NimBLECharacteristic *pCharacteristic;
+SemaphoreHandle_t xMutex;
+NimBLECharacteristic *pCharacteristic;
 float temperature = 25.0;
-static bool confirmationReceived = false;
+std::atomic<bool> confirmationReceived(false);
 
 // UUIDs for the service and characteristics
 #define SERVICE_UUID        "12345678-1234-5678-1234-56789abcdef0"
@@ -16,16 +15,23 @@ NimBLEServer *pServer;
 
 // Callback for receiving confirmations from the client
 class MyCallbacks: public NimBLECharacteristicCallbacks {
-    void onWrite(NimBLECharacteristic *pCharacteristic) {
-        std::string value = pCharacteristic->getValue();
-        std::string confirmation = value.substr(0, 3);
-        std::string dataValue = value.substr(3);
-        Serial.println(String(confirmation.c_str()));
-        Serial.println("LOOK HERE");
-        if (confirmation == "ACK" || confirmation == "NAK") {
-            confirmationReceived = true;
-            Serial.println("->Confirmation received: " + String(dataValue.c_str()) + "->" + String(confirmation.c_str()));
-        }
+    void onWrite(NimBLECharacteristic *pCharacteristic, NimBLEConnInfo &connInfo) {
+        if (xSemaphoreTake(xMutex, portMAX_DELAY) == pdTRUE) {
+          Serial.println("LOOK HERE ON WRITE");
+          std::string value = pCharacteristic->getValue();
+          std::string confirmation = value.substr(0, 3);
+          std::string dataValue = value.substr(3);
+          Serial.println(String(confirmation.c_str()));
+          
+          if (confirmation == "ACK" || confirmation == "NAK") {
+              // confirmationReceived = true;
+              
+              confirmationReceived.store(true, std::memory_order_release); 
+            }
+              Serial.println("->Confirmation received: " + String(dataValue.c_str()) + "->" + String(confirmation.c_str()));
+          }
+        xSemaphoreGive(xMutex);
+
     }
 };
 
@@ -33,22 +39,28 @@ void return_BLE(){
   float special_val = 20.0;
   pCharacteristic->setValue(special_val);
   pCharacteristic->notify();
+
 }
 
 void Start_BLE_server_NIMBLE(void* params) {
-    NimBLEDevice::init("ESP32_Temperature_Server");
+    Serial.begin(115200);
+    delay(500);
+
+
+    NimBLEDevice::init("ESP32_Touch_screen_Server");
     pServer = NimBLEDevice::createServer();
 
     NimBLEService *pService = pServer->createService(SERVICE_UUID);
     pCharacteristic = pService->createCharacteristic(
                         CHARACTERISTIC_UUID,
-                        NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::NOTIFY
+                        NIMBLE_PROPERTY::READ | NIMBLE_PROPERTY::WRITE | NIMBLE_PROPERTY::NOTIFY | NIMBLE_PROPERTY::INDICATE  
                     );
 
     pCharacteristic->setCallbacks(new MyCallbacks());
     pService->start();
 
-    NimBLEAdvertising *pAdvertising = NimBLEDevice::getAdvertising();
+    NimBLEAdvertising *pAdvertising = pServer->getAdvertising();
+
     pAdvertising->start();
     while (1) {
         // temperature += 0.1; // Simulate temperature change
@@ -56,18 +68,11 @@ void Start_BLE_server_NIMBLE(void* params) {
         // pCharacteristic->notify();
         // Serial.print("Send:" + String(temperature));
         // if (!confirmationReceived) {
-        //   Serial.println("NO");
+        //   // Serial.println("NO");
         // }
         // else {
         //   Serial.println("YES");
         // }
-      if (!confirmationReceived) {
-        //Serial.println("NO");
-      }
-      else {
-        Serial.println("YES");
-        break;
-      }
         delay(2000);
     }
 }
